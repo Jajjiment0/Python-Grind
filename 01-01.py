49293803a50fda09c8e7c485960883c5c2df8c1d
เนม บรรจบ <m.bunjobofficial@gmail.com>
2022-09-17 03:18:19 +0700

    exercise

--- 01-01.py
+++ 01-01.py
@@ -1,1 +1,7 @@
-print("Hello")
+import math
+a = math.pi - math.factorial(10)/8**8
+b = 7/math.sqrt(71) - math.sin(math.radians(40))
+c = math.log( 9.7 , math.e)**b
+d = 1.2**2.3**(1/3)
+e = (a+c)/d
+print(round(e, 6))
